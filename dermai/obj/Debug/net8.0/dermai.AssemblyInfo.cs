3a8fcb612e72d7343799b9b08078c5cc1d2083b4
valen <48855001@est.ort.edu.ar>
2025-11-20 10:56:35 -0300

    correcion un poco de arte

--- dermai/obj/Debug/net8.0/dermai.AssemblyInfo.cs
+++ dermai/obj/Debug/net8.0/dermai.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dermai")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce1d9daa2b963dad541223bd6dc8e8e78e092eaa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c77d99c7e8c7173ebd255f38085550f17e253d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("dermai")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dermai")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
